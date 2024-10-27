db10baf6ca7e6cf6e452e491884bbfe1526a2d5b
ericBlack1 <ericangeloawa@gmail.com>
2024-10-27 05:50:09 +0100

    cloud connection

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const serviceController = require("../controllers/serviceController");
-const upload = require('../config/uploadConfig');
+const upload = require('../config/cloudinaryConfig');
 
 /**
  * @swagger
